9710156d9b7d51936d3444eb4b75e75509df149e
michael <mvmnr01@gmail.com>
2026-09-05 12:52:27 -0500

    script skeleton

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLib = [];
+
+function Book(id, title, author) {
+    // constructor
+}
+
+function addBookToLib(id, title, author){
+    /* take params, create book,
+       then store in array */
+}
