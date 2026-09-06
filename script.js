1e78b0442c929f6da6120a9334aff241943b37e3
michael <mvmnr01@gmail.com>
2026-09-06 13:01:14 -0500

    read status functionality completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLib = [];
 
-function Book(id, title, author) {
+function Book(id, title, author, read) {
     // constructor
     if(!new.target){
         throw Error("You must use the 'new' operator to call the constructor");
@@ -8,13 +8,14 @@ function Book(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
+    this.read = read;
 }
 
-function addBookToLib(title, author){
+function addBookToLib(title, author, read){
     /* take params, create book,
     then store in array */
-    id = crypto.randomUUID()
-    const book = new Book(id,title,author);
+    id = crypto.randomUUID();
+    const book = new Book(id,title,author,read);
     myLib.push(book);
 }
 
@@ -27,6 +28,10 @@ function displayBookTitles(){
                 <td>${book.id}</td>   
                 <td>${book.title}</td>  
                 <td>${book.author}</td>
+                <td>
+                    <button class="btn btn-primary" onclick="readBook('${book.id}')">Read</button>
+                    <p>${book.read}</p>
+                </td>
                 <td>
                     <button class="btn btn-danger" onclick="removeEntry('${book.id}')">Remove</button>
                 </td>
@@ -45,9 +50,10 @@ function processNewBook(event) {
     // grab values from html
     title = document.getElementById('bookTitle').value;
     author = document.getElementById('bookAuthor').value;
+    read = false;
 
     // create new book entry
-    addBookToLib(title,author);
+    addBookToLib(title,author,read);
 
     // display titles
     displayBookTitles();
@@ -56,5 +62,14 @@ form.addEventListener('submit', processNewBook);
 
 function removeEntry(id){
     myLib = myLib.filter(book => book.id !== id);
+    displayBookTitles();
+}
+
+function readBook(id){
+    myLib = myLib.map(book =>
+        book.id === id
+            ? {...book, read: !book.read}
+            : book
+    );
     displayBookTitles();
 }
